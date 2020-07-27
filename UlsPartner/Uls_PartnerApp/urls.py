01804bb444e144ccc4e7d7e3e931046afe476a1a
irfan khan <itikpirfan@gmail.com>
2020-07-27 17:43:06 +0530

    terms & privacy updated

--- UlsPartner/Uls_PartnerApp/urls.py
+++ UlsPartner/Uls_PartnerApp/urls.py
@@ -4,4 +4,7 @@ from Uls_PartnerApp import views
 
 urlpatterns = [
     path('',views.home_view, name = 'home'),
+    path('terms-conditions', views.Terms_Conditions, name = 'terms_conditions'),
+    path('privacy-policy', views.privacy_policy, name = 'pravacy_policy')
+
 ]
--- UlsPartner/Uls_PartnerApp/views.py
+++ UlsPartner/Uls_PartnerApp/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render, redirect
 
 
 def home_view(request):
-
     template = 'Uls_PartnerApp/home.html'
     return render(request, template)
 
+
+def Terms_Conditions(request):
+    template_name = 'Uls_PartnerApp/terms_conditions.html'
+    return render(request, template_name)
+
+
+def privacy_policy(request):
+    template_name = 'Uls_PartnerApp/privacy_policy.html'
+    return render(request, template_name)
